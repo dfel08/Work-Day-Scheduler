f54af6cce6e030679ab6b089eedb5a52163894d2
dfel08 <dfellows68@gmail.com>
2020-03-25 20:31:14 -0700

    isBefore working, but date not refreshing

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ var now = moment().toDate();
 $(".currentDay").append("<h3>" + now + "</h3>");
 //In moment.js, use isBefore function to set calendar hour to gray
 function before7() {
-    moment().isBefore("T19:00:00");
-    console.log(Boolean);
+    var isItWorking = moment(now).isBefore("2020-03-25 20:00:00");
+    console.log(isItWorking);
 }
 
 before7();
-
+//refresh the date daily
 //Use isSame function to set hour to red
 //isAfter to set to green
