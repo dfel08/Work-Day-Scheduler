a69927cb0d7e449a720328d026a654b78210110a
dfel08 <dfellows68@gmail.com>
2020-03-25 18:52:17 -0700

    added calendar draft and began moment code/stuck

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 //Create a variable to store current time
 var now = moment().toDate();
 //Append that variable to the main tag in HTML
-$(".currentTime").append("<h3>" + now + "</h3>");
-//create a table with 
+$(".currentDay").append("<h3>" + now + "</h3>");
 //In moment.js, use isBefore function to set calendar hour to gray
+function before7() {
+    moment().isBefore("T19:00:00");
+    console.log(Boolean);
+}
+
+before7();
+
 //Use isSame function to set hour to red
 //isAfter to set to green
