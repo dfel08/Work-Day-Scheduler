dae02dd8727b6c5c69d0c8f50e6f71435b9521cc
dfel08 <dfellows68@gmail.com>
2020-03-25 17:26:58 -0700

    Appended current date/time to top of page

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//Create a variable to store current time
+var now = moment().toDate();
+//Append that variable to the main tag in HTML
+$(".currentTime").append("<h1>" + now + "</h1>");
 //In moment.js, use isBefore function to set calendar hour to gray
 //Use isSame function to set hour to red
 //isAfter to set to green
