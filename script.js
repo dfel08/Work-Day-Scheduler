8d542746af0f5f7b631ef5d336bb2ddc593415e2
dfel08 <dfellows68@gmail.com>
2020-03-25 17:32:48 -0700

    Added header to html

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //Create a variable to store current time
 var now = moment().toDate();
 //Append that variable to the main tag in HTML
-$(".currentTime").append("<h1>" + now + "</h1>");
+$(".currentTime").append("<h3>" + now + "</h3>");
+//create a table with 
 //In moment.js, use isBefore function to set calendar hour to gray
 //Use isSame function to set hour to red
 //isAfter to set to green
