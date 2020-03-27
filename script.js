d063632d8e9be62935d02b77c8f7913b76c5972b
dfel08 <dfellows68@gmail.com>
2020-03-27 15:24:15 -0700

    edit localStorage and previous hour to red

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function hour11 () {
     var currentTime = moment().format("HH");
     if (currentTime < "11") {
         $("#11am").css("backgroundColor", "lime");
-    } else if (currentTime = "11") {
+    } else if (currentTime === "11") {
         $("#11am").css("backgroundColor", "lightgray");
     } else {
         $("#11am").css("backgroundColor", "red");
@@ -40,7 +40,7 @@ function hour12 () {
     var currentTime = moment().format("HH");
     if (currentTime < "12") {
         $("#12pm").css("backgroundColor", "lime");
-    } else if (currentTime = "12") {
+    } else if (currentTime === "12") {
         $("#12pm").css("backgroundColor", "lightgray");
     } else {
         $("#12pm").css("backgroundColor", "red");
@@ -51,7 +51,7 @@ function hour13 () {
     var currentTime = moment().format("HH");
     if (currentTime < "13") {
         $("#1pm").css("backgroundColor", "lime");
-    } else if (currentTime = "13") {
+    } else if (currentTime === "13") {
         $("#1pm").css("backgroundColor", "lightgray");
     } else {
         $("#1pm").css("backgroundColor", "red");
@@ -62,7 +62,7 @@ function hour14 () {
     var currentTime = moment().format("HH");
     if (currentTime < "14") {
         $("#2pm").css("backgroundColor", "lime");
-    } else if (currentTime = "14") {
+    } else if (currentTime === "14") {
         $("#2pm").css("backgroundColor", "lightgray");
     } else {
         $("#2pm").css("backgroundColor", "red");
@@ -73,7 +73,7 @@ function hour15 () {
     var currentTime = moment().format("HH");
     if (currentTime < "15") {
         $("#3pm").css("backgroundColor", "lime");
-    } else if (currentTime = "15") {
+    } else if (currentTime === "15") {
         $("#3pm").css("backgroundColor", "lightgray");
     } else {
         $("#3pm").css("backgroundColor", "red");
@@ -84,7 +84,7 @@ function hour16 () {
     var currentTime = moment().format("HH");
     if (currentTime < "16") {
         $("#4pm").css("backgroundColor", "lime");
-    } else if (currentTime = "16") {
+    } else if (currentTime === "16") {
         $("#4pm").css("backgroundColor", "lightgray");
     } else {
         $("#4pm").css("backgroundColor", "red");
@@ -101,8 +101,22 @@ $('.save').on('click', function(){
 });
 
 function readVal() {
-    var nineAm = localStorage.getItem("9am")
-    $("#9am").val(nineAm)
+    var nineAm = localStorage.getItem("9am");
+    tenAm = localStorage.getItem("10am");
+    elevenAm = localStorage.getItem("11am");
+    twelvePm = localStorage.getItem("12pm");
+    onePm = localStorage.getItem("1pm");
+    twoPm = localStorage.getItem("2pm");
+    threePm = localStorage.getItem("3pm");
+    fourPm = localStorage.getItem("4pm");
+    $("#9am").val(nineAm);
+    $("#10am").val(tenAm);
+    $("#11am").val(elevenAm);
+    $("#12pm").val(twelvePm);
+    $("#1pm").val(onePm);
+    $("#2pm").val(twoPm);
+    $("#3pm").val(threePm);
+    $("#4pm").val(fourPm);
 }
 
 hour9();
